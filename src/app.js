9d031814ce5117c4670c45710699d7ad194ba4ad
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2022-01-05 12:50:52 +0100

    fifth commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const { authRouter, userRouter } = require("../routes");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.static("public"));
